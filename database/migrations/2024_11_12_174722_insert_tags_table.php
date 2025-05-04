1e05fa00325d8310007458539410b8e93c896c52
Richard Oliveira <richarok18@gmail.com>
2025-05-04 12:11:44 -0300

    Remoção tags

--- database/migrations/2024_11_12_174722_insert_tags_table.php
+++ database/migrations/2024_11_12_174722_insert_tags_table.php
@@ -1,4 +1,4 @@
 <?php
 
 $sql = 'INSERT INTO tags (name)
-VALUES ("Não iniciado"), ("Em andamento"), ("Concluído"), ("Segurança"), ("TI"), ("Limpeza"), ("Estrutura"), ("Alerta Geral")';
+VALUES ("Segurança"), ("TI"), ("Limpeza"), ("Estrutura"), ("Alerta Geral")';
