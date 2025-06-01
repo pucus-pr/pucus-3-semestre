6f9cdcd06319537a79db3701197f7977f51a3ac9
Richard Oliveira <richarok18@gmail.com>
2025-06-01 17:38:10 -0300

    Votos terminados

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -16,20 +16,57 @@ class Vote extends Model {
     public static function create($request) {
         array_unshift($request, $_SESSION['user']);
 
-        $sql = 'INSERT INTO votes (user_id, post_id, value) VALUES (?, ?, ?)';
-        $id = self::query($sql, $request);
-        return [
-            'status' => 'success',
-            'message' => 'Voto criado com sucesso!',
-            'data' => [
-                'id' => $id
-            ]
-        ];
+        $sql = 'SELECT * from votes WHERE user_id = ? AND post_id = ?';
+        $votes = self::query($sql, [$_SESSION['user'], $request['post_id']]);
+
+        if (count($votes) != 0) {
+            $vote = $votes[0];
+
+            if ($request['value'] == $vote['value']) {
+                Vote::delete($vote['id']);
+                $sql = 'SELECT COUNT(*) AS total FROM votes WHERE post_id = ? AND value = ?';
+                $quantity = self::query($sql, [$request['post_id'], $request['value']]);
+                return [
+                    'status' => 'success',
+                    'message' => 'Voto modificado com sucesso!',
+                    'data' => [
+                        'id' => $vote['id'],
+                        'quantity' => $quantity
+                    ]
+                ];
+            } else {
+                Vote::update([$request['value']], $vote['id']);
+                $sql = 'SELECT COUNT(*) AS total FROM votes WHERE post_id = ? AND value = ?';
+                $quantity = self::query($sql, [$request['post_id'], $request['value']]);
+                return [
+                    'status' => 'success',
+                    'message' => 'Voto modificado com sucesso!',
+                    'data' => [
+                        'id' => $vote['id'],
+                        'quantity' => $quantity
+                    ]
+                ];
+            }
+        } else {
+            $sql = 'INSERT INTO votes (user_id, post_id, value) VALUES (?, ?, ?)';
+            $id = self::query($sql, $request);
+
+            $sql = 'SELECT COUNT(*) AS total FROM votes WHERE post_id = ? AND value = ?';
+            $quantity = self::query($sql, [$request['post_id'], $request['value']]);
+            return [
+                'status' => 'success',
+                'message' => 'Voto criado com sucesso!',
+                'data' => [
+                    'id' => $id,
+                    'quantity' => $quantity
+                ]
+            ];
+        }
     }
 
     public static function update($request, $id) {
         $sql = 'UPDATE votes SET value = ? WHERE id = ?';
-        $request['id'] = $id;
+        array_push($request, $id);
         $id = self::query($sql, $request);
         return [
             'status' => 'success',
